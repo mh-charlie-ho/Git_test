4076d0d921c5413682f8bce1cfcb78f1cddcacbd
Ho, Ming-Hsin <charlie.ho@itri.org.tw>
2024-07-29 13:23:49 +0800

    version3

--- Test.py
+++ Test.py
@@ -6,3 +6,6 @@ print('This is the version three')
 print('This is the version four')
 print('This is the version four')
 print('This is the version two')
+print('This is the version two')
+
+print('This is the version three')
